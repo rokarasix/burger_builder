3cb152c18cdabe44d270b2647cb0dce6b0a12876
rajni <rajni.dasari@shopx.in>
2020-08-04 22:51:33 +0530

    add buttons and backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,8 @@ class BurgerBuilder extends Component{
             meat:0
         },
         totalPrice : 4 ,
-        purchasable : false 
+        purchasable : false ,
+        purchasing : false
     }
 
     updatePurchaseState (ingredients) {
@@ -69,6 +70,19 @@ class BurgerBuilder extends Component{
         this.updatePurchaseState(updateIngredients);
     }
 
+    purchaseHandler = () =>{
+        this.setState({purchasing : true});
+    }
+
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing : false});
+    }
+
+    purchaseContinueHandler = () =>{
+        alert('You continue!');
+        this.setState({purchasing : true});
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -80,8 +94,11 @@ class BurgerBuilder extends Component{
 
         return(
             <Aux>
-                <Modal>
-                    <OrderSummary ingredients = {this.state.ingredients}/>
+                <Modal show={this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
+                    <OrderSummary 
+                        ingredients = {this.state.ingredients}
+                        purchaseCancelled = {this.purchaseCancelHandler}
+                        purchaseContinued = {this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls 
@@ -89,6 +106,7 @@ class BurgerBuilder extends Component{
                     ingredientRemoved = {this.removeIngredientHandler}
                     disabled = {disabledInfo}
                     purchasable = {this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     price = {this.state.totalPrice}/>
             </Aux>
         );
