5c1f0cc62f2b4dfbdd248c2b4d9e4073f5050b24
rajni <rajni.dasari@shopx.in>
2020-11-20 15:01:51 +0530

    improving performance

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,11 +2,21 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
 
+    let attachedClasses = [classes.SideDrawer,classes.Close];
+
+    if(props.open){
+        attachedClasses = [classes.SideDrawer,classes.Open];
+    }
+
     return(
-        <div className = {classes.SideDrawer}>
+        <Aux>
+            <Backdrop show={props.open} clicked = {props.closed}/>
+            <div className = {attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
@@ -14,6 +24,8 @@ const sideDrawer = (props) => {
                 <NavigationItems/>
             </nav>
         </div>
+
+        </Aux>
     );
 };
 
